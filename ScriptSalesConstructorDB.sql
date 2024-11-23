966b84591577efc68ad8bc29ab98d7e7d667c204
JSamuraiL <165559604+JSamuraiL@users.noreply.github.com>
2024-11-23 04:46:39 +0300

    Update ScriptSalesConstructorDB.sql
    БД изменена с учетом контекста кода

--- ScriptSalesConstructorDB.sql
+++ ScriptSalesConstructorDB.sql
@@ -1,17 +1,10 @@
-CREATE TABLE roles 
-(
-	id SERIAL PRIMARY KEY,
-	role VARCHAR(20) NOT NULL
-);
-
 CREATE TABLE managers (
     	id UUID PRIMARY KEY,
 	mail VARCHAR(255) NOT NULL,
 	hashed_password VARCHAR(255) NOT NULL,
     	name VARCHAR(35) NOT NULL,
 	surname VARCHAR(35) NOT NULL,
-	patronymic VARCHAR(35) NOT NULL,
-    	role_id SERIAL REFERENCES roles(id) NOT NULL
+	patronymic VARCHAR(35) NOT NULL
 );
 
 CREATE TABLE sellers 
@@ -21,22 +14,13 @@ CREATE TABLE sellers
 	hashed_password VARCHAR(255) NOT NULL,
 	name VARCHAR(35) NOT NULL,
 	surname VARCHAR(35) NOT NULL,
-	patronymic VARCHAR(35) NOT NULL,
-	role_id SERIAL REFERENCES roles(id) NOT NULL, 
+	patronymic VARCHAR(35) NOT NULL, 
 	manager_id UUID REFERENCES managers(id)
 ); 
-
-CREATE TABLE script_categories
-(
-	id SERIAL PRIMARY KEY NOT NULL,
-	title VARCHAR(50) NOT NULL,
-	description TEXT
-);
 
 CREATE TABLE scripts
 (
 	id SERIAL PRIMARY KEY NOT NULL,
-	category_id SERIAL REFERENCES script_categories(id),
 	title VARCHAR(50) NOT NULL,
 	description TEXT,
 	creator_id UUID REFERENCES managers(id) NOT NULL
@@ -49,17 +33,9 @@ CREATE TABLE blocks
 	content TEXT
 );
 
-CREATE TABLE block_connection_types
-(
-	id SERIAL PRIMARY KEY NOT NULL,
-	title VARCHAR(50) NOT NULL,
-	content TEXT
-);
-
 CREATE TABLE block_connections
 (
 	id SERIAL PRIMARY KEY NOT NULL,
-	block_connection_type_id SERIAL REFERENCES block_connection_types(id) NOT NULL,
 	previous_block_id SERIAL REFERENCES blocks(id) NOT NULL,
 	next_block_id SERIAL REFERENCES blocks(id) NOT NULL
 )
