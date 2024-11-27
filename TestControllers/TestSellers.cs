b1402902e59e8bfdecd79e2715281855cd849657
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-11-27 20:34:33 +0300

    Создан тест для продавцов на проверку корректно введенного значения

--- TestControllers/TestSellers.cs
+++ TestControllers/TestSellers.cs
@@ -5,6 +5,7 @@ using SalesScriptConstructor.Domain.Interfaces;
 using SalesScriptConstructor.Domain.Interfaces.ISellers;
 using Castle.Core.Logging;
 using Microsoft.Extensions.Logging;
+using SalesScriptConstructor.Domain.Entities;
 
 namespace TestControllers
 {
@@ -24,8 +25,20 @@ namespace TestControllers
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public async Task GetLinkedSellers_ReturnsSellers_WhenManagerExists()
         {
+            //Arrange
+            var managerId = Guid.NewGuid();
+            var sellers = new List<Seller> {new Seller {Id = Guid.NewGuid(), Name = "string"}};
+            _mockSellersService.Setup( s => s.GetSellersByManagerId(managerId)).ReturnsAsync(sellers);
+
+            //Act
+            var result = await _controller.GetLinkedSellers(managerId);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<Seller>));
+            var okResult = result as IEnumerable<Seller>; 
+            Assert.AreEqual(1, okResult.Count());
         }
     }
 }
