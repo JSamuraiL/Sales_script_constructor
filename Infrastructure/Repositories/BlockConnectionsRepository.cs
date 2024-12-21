97eaafde7bc8547a2ac72cdde25468c6c60fc70a
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-21 04:45:26 +0300

    Изменение способа запроса: используется SQL запрос без Entity Framework

--- Infrastructure/Repositories/BlockConnectionsRepository.cs
+++ Infrastructure/Repositories/BlockConnectionsRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using SalesScriptConstructor.Domain.Entities;
 using SalesScriptConstructor.Domain.Interfaces.IBlockConnections;
 using System;
@@ -11,31 +12,78 @@ namespace SalesScriptConstructor.Infrastructure.Repositories
 {
     public class BlockConnectionsRepository : IBlockConnectionsRepository
     {
-        private readonly PostgreDbContext _dbContext;
-        public BlockConnectionsRepository (PostgreDbContext dbContext) 
+        private readonly string _connectionString;
+
+        public BlockConnectionsRepository(string connectionString)
         {
-            _dbContext = dbContext ?? throw new ArgumentNullException();
+            _connectionString = connectionString ?? throw new ArgumentNullException();
         }
+
         public async Task AddBlockConnectionAsync(BlockConnection blockConnection)
         {
-            await _dbContext.Database.ExecuteSqlAsync
-                ($"INSERT INTO block_connections (id, previous_block_id, next_block_id) VALUES ({blockConnection.Id}, {
-                    blockConnection.PreviousBlockId}, {blockConnection.NextBlockId})");
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new NpgsqlCommand("INSERT INTO block_connections (id, previous_block_id, next_block_id) VALUES (@id, @previousBlockId, @nextBlockId)", connection))
+                {
+                    command.Parameters.AddWithValue("id", blockConnection.Id);
+                    command.Parameters.AddWithValue("previousBlockId", blockConnection.PreviousBlockId);
+                    command.Parameters.AddWithValue("nextBlockId", blockConnection.NextBlockId);
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
         }
 
         public bool BlockConnectionExists(int id)
         {
-            return _dbContext.BlockConnections.FromSqlRaw($"SELECT * FROM sellers WHERE id='{id}'").Any();
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.OpenAsync();
+                using (var command = new NpgsqlCommand("SELECT COUNT(*) FROM block_connections WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("id", id);
+                    var count = (long) command.ExecuteScalar();
+                    return count > 0;
+                }
+            }
         }
 
         public async Task DeleteBlockConnectionAsync(int id)
         {
-            await _dbContext.Database.ExecuteSqlAsync($"DELETE FROM block_connections WHERE id='{id}'");
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new NpgsqlCommand("DELETE FROM block_connections WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("id", id);
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
         }
 
         public async Task<BlockConnection> GetBlockConnectionByIdAsync(int id)
         {
-            return await _dbContext.BlockConnections.FromSqlRaw($"SELECT * FROM block_connections WHERE id='{id}'").FirstOrDefaultAsync();
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new NpgsqlCommand("SELECT * FROM block_connections WHERE id = @id", connection))
+                {
+                    command.Parameters.AddWithValue("id", id);
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            return new BlockConnection
+                            {
+                                Id = (int)reader["id"],
+                                PreviousBlockId = (int)reader["previous_block_id"],
+                                NextBlockId = (int)reader["next_block_id"]
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
         }
     }
 }
